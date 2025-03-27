f44e331605c1ec5e2325aaf935dcc708148ad9e3
Matsu-J <matsu.jerkkola@gmail.com>
2025-03-27 14:37:47 +0200

    Updated database handling module

--- db.py
+++ db.py
@@ -2,13 +2,23 @@ import sqlite3
 from flask import g
 
 def connect_database():
-    db = sqlite3.connect("database.db")
-    db.execute("PRAGMA foreign_keys = ON")
-    db.row_factory = sqlite3.Row
-    return db
+    database = sqlite3.connect("database.db")
+    database.execute("PRAGMA foreign_keys = ON")
+    database.row_factory = sqlite3.Row
+    return database
 
-def execute():
-    db = connect_database()
-    result = db.execute(sql, params)
-    db.commit
-    g.last
+def execute(sql, parameters=[]):
+    database = connect_database()
+    result = database.execute(sql, parameters)
+    database.commit
+    g.last_insert_id = result.lastrowid
+    database.close()
+
+def last_insert_id():
+    return g.last_insert_id
+
+def query(sql, parameters=[]):
+    database = connect_database()
+    result = execute(sql, parameters).fetchall()
+    database.close()
+    return result

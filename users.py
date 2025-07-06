40c1be8b4ccd1fb6606d917e25bab0f0508a35f2
Matsu-J <matsu.jerkkola@gmail.com>
2025-07-06 14:32:45 +0300

    Fixed bug where the length didn't show up on posts

--- users.py
+++ users.py
@@ -33,7 +33,7 @@ def average_rating(user_id):
 
 
 def get_posts(user_id):
-    return db.query("""SELECT p.id, p.user_id, p.title, p.release_year, p.movie_hours, p.movie_minutes, p.rating, p.edited_at, u.id, u.username
+    return db.query("""SELECT p.id, p.user_id, p.title, p.release_year, p.movie_hours, p.movie_minutes, p.rating, p.created_at, p.edited_at, u.id, u.username
                      FROM posts p, users u 
                      WHERE u.id = p.user_id
                      AND p.user_id = ?
